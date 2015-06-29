b11c96081b5c51eeafcf50c0beb4f5cbf7b57d48
MKS-students <mks.students@hackreactor.com>
2015-06-29 16:11:11 -0500

    Passing all tests

---
+++ client/scripts/app.js
@@ -0,0 +1,117 @@
+var app = {
+  room : 'default',
+  friends : {}
+};
+app.init = function(){
+  app.fetch()
+  //Refresh button clears the page and fetches update
+  $('.refresh').on('click', function() {
+    app.fetch();
+  });
+
+  $('.input').focus().select();
+
+  $('#submit_message').on('click', function() {
+    app.send(app.createMessage($('.input').val()));
+  });
+
+  $('.chatrooms').on('click', function() {
+    $('.message').hide();
+    $($(event.target).attr('class')).show();
+    app.room = $(event.target).attr('class').slice(1).replace(/[^A-Za-z0-9]/g,'');
+    $('#current_chat').text(app.room)
+  })
+
+  $('#chatroom_submit').on('click', function() {
+    app.room = $('#chatroom_input').val().replace(/[^A-Za-z0-9]/g,'');
+    $('.message').hide();
+    $('.'+app.room).show();
+    $('#current_chat').text('Your current chatroom is '+app.room)
+  })
+}
+
+app.fetch = function() {
+  app.clearMessages();
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'http://127.0.0.1:3000/',
+  type: 'GET',
+  // data: JSON.stringify(message),
+  // contentType: 'application/json',
+  success: function (data) {
+    _.each(data['results'], function (post){
+      //Sanitize messages
+      var text = bleach.sanitize(post.text);
+      var user = bleach.sanitize(post.username).replace(/[^A-Za-z0-9]/g,'');
+      var room = bleach.sanitize(post.roomname).replace(/[^A-Za-z0-9]/g,'');
+
+      //Remove empty messages
+      if(!(user === 'undefined' || text === 'undefined' || user === '' || text === '')) {
+        //Appends messages to a empty div inside main
+        $('#messageHanger').append("<div id = chats username = "+user+" class = \"message "+room+" "+user+"\">"+user+":"+text+"  </div>").attr(user)
+        //Toggles friends on click
+        $('.message').on('click', function() {
+          app.room = $(event.target).attr('username').replace(/[^A-Za-z0-9]/g,'');
+          if ($(event.target).hasClass('friend')) {
+            app.friends[$(event.target).attr('username')] = false;
+            $('.'+$(event.target).attr('username')).removeClass('friend');
+          } else {
+            app.friends[$(event.target).attr('username')] = true;
+            $('.'+$(event.target).attr('username')).addClass('friend');
+          }
+        })
+      }
+      //Adds room buttons
+      if(room !== 'undefined' && document.getElementById('#'+room) === null && room !== '') {
+        $('.chatrooms').append("<button type = button id = #"+room+" class = ."+room+">"+room+"</button>")
+      }
+    })
+    console.log('chatterbox: Message recieved');
+  },
+  error: function (data) {
+    console.error('chatterbox: Failed to recieve');
+  }
+})};
+
+app.createMessage = function(text) {
+  var message = {}
+
+  message.username = window.location.search.slice(10);
+  message.text = text;
+  message.roomname = app.room;
+
+  return message;
+}
+
+app.clearMessages = function() {
+  $('.message').remove();
+}
+
+app.addMessage = function (message) {
+  var text = bleach.sanitize(message.text);
+  var user = bleach.sanitize(message.username);
+  if(!(user === 'undefined' || text === 'undefined')) {
+    $('body').append("<div id = chats>"+user+":"+text+"</div>")
+  }
+}
+
+app.send = function(message) {
+  $.ajax({
+    url: 'http://127.0.0.1:3000/',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+  app.fetch();
+}
+
+$(document).ready(function(){
+  app.init();
+})
--- server/request-handler.js
+++ server/request-handler.js
@@ -12,58 +12,66 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+var messages = [{name:'Colin', message:'I love chatterbox', room: 'Default'}];
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log("Serving request type " + request.method + " for url " + request.url);
-
-  // The outgoing status.
+exports.requestHandler = function(request, response) {
   var statusCode = 200;
-
-  // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
 
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
+  // console.log("Serving request type " + request.method + " for url " + request.url);
 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  if(request.method === 'GET'){
+    // General get request handler
+    if(request.url !== '/classes/room1' && request.url !== '/classes/messages'){
+      statusCode = 404;
+      headers['Content-Type'] = "text/plain";
+      response.writeHead(statusCode, headers);
+      response.end('Not found');
+    }else{
+      headers['Content-Type'] = "application/json";
+      statusCode = 200;
+      response.writeHead(statusCode, headers)
+      var data = {
+        results: messages
+      };
+      response.end(JSON.stringify(data));
+    }
 
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end("Hello, World!");
+  }else if(request.method === 'POST'){
+    // General POST request handler
+    statusCode = 201;
+    response.writeHead(statusCode, headers)
+    headers['Content-Type'] = "text/plain";
+
+    if(request.url === '/classes/room1' || request.url === '/classes/messages'){
+      var body = "";
+
+      request.on('data', function(data){ body += data});
+      request.on('end', function(){
+        var parsed = JSON.parse(body);
+
+        if(!parsed.username || !parsed.message){
+          // If no username or text supplied
+          statusCode = 500;
+          response.writeHead(statusCode, headers)
+          response.end('Invalid data type');
+        }else{
+          // If data is okay
+          messages.unshift(parsed)
+          response.end('Message received!');
+        }
+      })
+    }
+  } else{
+    // 404 Request handler
+    statusCode = 404;
+    headers['Content-Type'] = "text/plain";
+    response.writeHead(statusCode, headers);
+    response.end('Not found');
+  }
+
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
